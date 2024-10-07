30da69ab102683b929b3a9467a6d250db33a9674
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-10-07 21:24:33 +0700

    proxy updated for deploy

--- todo_frontend/src/interceptor/axiosInstance.js
+++ todo_frontend/src/interceptor/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "/",
+    baseURL: "https://naingkhanthtet.pythonanywhere.com",
     headers: {
         "Content-Type": "application/json",
     },
